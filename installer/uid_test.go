2d56c98d444da3b4254c0844da8d3a93bad6178b
Fabio Forni <fabio.forni@mta.it>
2023-01-03 16:15:04 +0100

    installer: Test uid.go

---
+++ installer/uid_test.go
@@ -0,0 +1,37 @@
+//go:build unix
+
+package installer_test
+
+import (
+	"testing"
+	"testing/fstest"
+
+	"github.com/livingsilver94/backee/installer"
+	"golang.org/x/sys/unix"
+)
+
+func TestIDFS(t *testing.T) {
+	const expUID = 123
+	const expGID = 456
+	fs := fstest.MapFS{
+		"file.txt": &fstest.MapFile{Sys: unix.Stat_t{Uid: expUID, Gid: expGID}},
+	}
+
+	uid, gid, err := installer.IDFS(fs, "file.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if uid != expUID || gid != expGID {
+		t.Fatalf("expected UID %d and GID %d. Got %d and %d", expUID, expGID, uid, gid)
+	}
+}
+
+func TestRunAs(t *testing.T) {
+	f := func() error { return nil }
+	uid := unix.Getuid()
+	gid := unix.Getgid()
+	err := installer.RunAs(f, uid, gid)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
