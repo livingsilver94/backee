7419eecce0e2d3ceedc7ba9553de383701ff115b
Fabio Forni <development@redaril.me>
2023-01-07 15:25:23 +0100

    installer: Fix build on Windows

--- installer/uid_windows.go
+++ installer/uid_windows.go
@@ -2,12 +2,17 @@
 
 package installer
 
+import (
+	"io/fs"
+	"os"
+)
+
 func UnixIDsFS(sys fs.FS, path string) (uid int, gid int, err error) {
 	return -1, -1, nil
 }
 
 func UnixIDs(path string) (uid int, gid int, err error) {
-	return IDFS(os.DirFS(path), ".")
+	return UnixIDsFS(os.DirFS(path), ".")
 }
 
 func RunAs(f func() error, uid, gid int) error {
