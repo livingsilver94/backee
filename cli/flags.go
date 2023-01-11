cb474af3f7a3bfcaa8724fcb3cded468a4296367
Fabio Forni <fabio.forni@mta.it>
2023-01-11 11:39:53 +0100

    cli: Add KeepassXC arguments

--- cli/flags.go
+++ cli/flags.go
@@ -6,10 +6,17 @@ import (
 	"github.com/alecthomas/kong"
 )
 
+type KeepassXC struct {
+	Path     string `env:"KEEPASSXC_PATH" help:"KeepassXC database path"`
+	Password string `env:"KEEPASSXC_PASSWORD" help:"KeepassXC database password"`
+}
+
 type Arguments struct {
 	Directory string `short:"C" type:"existingdir" help:"Change the base directory."`
 	Quiet     bool   `short:"q" help:"Do not print anything on the terminal."`
 	Variant   string `help:"Specify the system variant."`
+
+	KeepassXC KeepassXC `embed:"" prefix:"keepassxc."`
 
 	Services []string `arg:"" help:"Services to install."`
 }
