fa5bd71a5ed82c15f4971af298b537d17e3804c6
Fabio Forni <development@redaril.me>
2023-01-14 10:58:13 +0100

    secret: Show KeepassXC runtime errors

--- secret/keepassxc.go
+++ secret/keepassxc.go
@@ -1,9 +1,10 @@
 package secret
 
 import (
-	"bytes"
+	"errors"
 	"io"
 	"os/exec"
+	"strings"
 )
 
 type KeepassXC struct {
@@ -19,17 +20,22 @@ func NewKeepassXC(dbPath, password string) KeepassXC {
 }
 
 func (k KeepassXC) Value(key string) (string, error) {
-	cmd := exec.Command("keepassxc-cli", "show", "-q", "-sa", "password", k.dbPath, key)
+	cmd := exec.Command("keepassxc-cli", "show", "-sa", "password", k.dbPath, key)
 
 	in, err := cmd.StdinPipe()
 	if err != nil {
 		return "", err
 	}
 	go func() {
-		defer in.Close()
 		io.WriteString(in, k.password)
 	}()
 
 	value, err := cmd.Output()
-	return string(bytes.TrimSuffix(value, []byte{'\n'})), err
+	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			err = errors.New(strings.TrimSpace(string(exitErr.Stderr)))
+		}
+		return "", err
+	}
+	return strings.TrimSuffix(string(value), "\n"), nil
 }
