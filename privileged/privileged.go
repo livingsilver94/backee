c8e7d384dcf09b84f76d17b55b5ce54a60cbf92d
Fabio Forni <development@redaril.me>
2023-08-14 11:30:03 +0200

    privileged: Implement IPC messages

---
+++ privileged/privileged.go
@@ -0,0 +1,62 @@
+package privileged
+
+import (
+	"encoding/gob"
+	"io"
+)
+
+type WriterEncoder interface {
+	EncodeWriter(w io.Writer) error
+}
+
+type ReaderDecoder interface {
+	DecodeReader(r io.Reader) error
+}
+
+type Action int
+
+const (
+	LinkFile Action = iota + 1
+	CopyFile
+	WriteContent
+)
+
+type SourceDestMessage struct {
+	Source string
+	Dest   string
+}
+
+func (m *SourceDestMessage) EncodeWriter(w io.Writer) error {
+	enc := gob.NewEncoder(w)
+	return enc.Encode(m)
+}
+
+func (m *SourceDestMessage) DecodeReader(r io.Reader) error {
+	dec := gob.NewDecoder(r)
+	return dec.Decode(&m)
+}
+
+type WriteContentMessage struct {
+	Dest    string
+	content io.Reader
+}
+
+func (m *WriteContentMessage) EncodeWriter(w io.Writer) error {
+	enc := gob.NewEncoder(w)
+	err := enc.Encode(&m.Dest)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(w, m.content)
+	return err
+}
+
+func (m *WriteContentMessage) DecodeReader(r io.Reader) error {
+	dec := gob.NewDecoder(r)
+	err := dec.Decode(&m.Dest)
+	if err != nil {
+		return err
+	}
+	m.content = r
+	return err
+}
